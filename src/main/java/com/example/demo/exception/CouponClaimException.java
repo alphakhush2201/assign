e4994f5c550bbca455a552de36669f178d410858
alphakhush2201 <115687208+alphakhush2201@users.noreply.github.com>
2025-03-15 11:53:54 +0530

    removing unnecessary files

--- src/main/java/com/example/demo/exception/CouponClaimException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class CouponClaimException extends RuntimeException {
-    public CouponClaimException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/demo/service/CouponService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.dto.CouponResponse;
-
-public interface CouponService {
-    CouponResponse claimCoupon(String ip, String session);
-    long getRemainingTimeForNextClaim(String ip, String session);
-    boolean isEligibleForClaim(String ip, String session);
-}
